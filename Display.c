fd3de866f27c7ce93ee5d7d5cdf31b951f023c33
alinnecaastro <alinnecastro133@gmail.com>
2025-02-08 22:38:51 -0300

    fonte.h atualização arquivo

--- Display.c
+++ Display.c
@@ -15,12 +15,13 @@
 #include "font.h"
 #include "pico/bootrom.h"
 
-#define I2C_PORT i2c1
-#define I2C_SDA 14
-#define I2C_SCL 15
-#define endereco 0x3C
-
-bool led_verde = false;
+#define I2C_PORT i2c1 // Especifica qual porta I2C está sendo usada, no caso o i2c
+#define I2C_SDA 14 // Especifica I2C Data e conecta a GPIO 14
+#define I2C_SCL 15 // Especifica I2C Clock e conecta a GPIO 15
+#define endereco 0x3C // Define o endereço I2C do dispositivo com o qual
+                      //  voce esta se comunicando, sendo esse o displays OLEDm como o ssd1306
+//Declara variavel booleanas das leds
+bool led_verde = false; 
 bool led_azul = false;
 
 //Função principal
@@ -31,34 +32,43 @@ int main() {
     // Inicializa e Configura o LED Vermelho 
     gpio_init(GPIO_PIN_RED);
     gpio_set_dir(GPIO_PIN_RED, GPIO_OUT);
+    // Inicializa e Configura o LED Azul
     gpio_init(GPIO_PIN_BLUE);
     gpio_set_dir(GPIO_PIN_BLUE, GPIO_OUT);
+    // Inicializa e Configura o LED Verde
     gpio_init(GPIO_PIN_GREEN);
     gpio_set_dir(GPIO_PIN_GREEN, GPIO_OUT);
+    // Inicializa e Configura o Botão A e Pull-Up
     gpio_init(BUTTON_PIN_A);
     gpio_set_dir(BUTTON_PIN_A, GPIO_IN);
     gpio_pull_up(BUTTON_PIN_A);
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN_A, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);//    // Configura as interrupções para ambos os botões, usando a mesma callback.
-   
+    //// Configura as interrupções para ambos os botões, usando a mesma callback.
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN_A, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);   
     // Inicializa e Configura o Botão B e PUll-Up
     gpio_init(BUTTON_PIN_B);
     gpio_set_dir(BUTTON_PIN_B, GPIO_IN);
     gpio_pull_up(BUTTON_PIN_B);
     gpio_set_irq_enabled(BUTTON_PIN_B, GPIO_IRQ_EDGE_FALL, true); // Borda de descida apenas
 
+    // Inicaliza a comunicação I2C em um microcontrolador em 400kHz
     i2c_init(I2C_PORT, 400 * 1000);
 
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);//Configura o pino para funcionar como um pino I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);// Configura o pino para funcionar como o pino de relogio I2C, sincronizar a comunicação entre os dispositivos.
+    // Garanti que as linhas de comunicação I2C sejam mantidas em um nivel logico alto
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    ssd1306_t ssd;
+    ssd1306_t ssd;//Declara uma variavel do tipo ssd1306_t, representa o display.
+   // Inicializa o display, dimensões do display,parametro que especifica esta invertido ou nao, enderço I2C, a porta que esta sendo usada
     ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    // Aplica configurações adicionais ao display
     ssd1306_config(&ssd);
+    // Envia os dados configurados para o display
     ssd1306_send_data(&ssd);
 
     //Limpa o display
     ssd1306_fill(&ssd, false);
+    // Envia os dados configurados para o display
     ssd1306_send_data(&ssd);
 
 
@@ -102,8 +112,8 @@ int main() {
         ssd1306_fill(&ssd, false);//Limpa o display
         ssd1306_send_data(&ssd);
         // Exibe os status de ambos os LEDs no display
-        ssd1306_fill(&ssd,false);
-        ssd1306_draw_string(&ssd,"LEDS ",20,15);
+        ssd1306_fill(&ssd,false); //Limpa o display
+        ssd1306_draw_string(&ssd,"LEDS ",20,15);//Desenha uma string no display
         ssd1306_draw_string(&ssd, "LED verde: ", 20, 30);
         if (led_verde) {
             ssd1306_draw_string(&ssd, "ON", 100, 30); // LED verde está ligado
@@ -119,9 +129,10 @@ int main() {
         ssd1306_send_data(&ssd); // Atualiza o display
         ssd1306_fill(&ssd,false);
 
-    // Leitura de caracteres via UART
+        // Leitura de caracteres via UART
+        //Parametro (0) significa que nao ha tempo limite
         int c = getchar_timeout_us(0); // Lê um caractere do Serial Monitor
-        if (c != PICO_ERROR_TIMEOUT) {
+        if (c != PICO_ERROR_TIMEOUT) { //Verifica se a leitura nao ocorreu erro.
             // Limpa o display antes de exibir o novo caractere
             ssd1306_fill(&ssd, false);
             
